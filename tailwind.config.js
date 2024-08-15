5a5cb75771fe838389c700f0b3b216db4324dfbe
Oksana <axana13@gmail.com>
2024-08-15 08:29:30 +0200

    may be ignore

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html", // Include your root HTML file
+    "./src/**/*.css", // Include all CSS files in the src folder
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
